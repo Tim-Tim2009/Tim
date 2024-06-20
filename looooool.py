5b319b40e5bd58cb124ec1252a8361d7f7ba85b1
Tim-Tim2009 <alzutim@gmail.com>
2024-06-20 12:51:43 +0200

    Update looooool.py

--- looooool.py
+++ looooool.py
@@ -4,22 +4,6 @@ from openpyxl import load_workbook
 import os
 pygame.init()
 
-def save_game_data(file_name, data):
-    # Crée un DataFrame à partir des données de jeu
-    df = pd.DataFrame([data])
-
-    # Si le fichier existe, ouvre-le et ajoute les données, sinon crée un nouveau fichier
-    if os.path.exists(file_name):
-        book = load_workbook(file_name)
-        writer = pd.ExcelWriter(file_name, engine='openpyxl')
-        writer.book = book
-        writer.sheets = {ws.title: ws for ws in book.worksheets}
-        for sheetname in writer.sheets:
-            df.to_excel(writer, sheet_name=sheetname, startrow=writer.sheets[sheetname].max_row, index=False, header=False)
-        writer.save()
-    else:
-        df.to_excel(file_name, index=False)
-
 WIDTH, HEIGHT = 800, 600
 
 FEN = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -28,9 +12,11 @@ pygame.display.set_caption("Aim Trainer")
 TARGET_INCREMENT = 800
 TARGET_EVENT = pygame.USEREVENT
 
-TARGET_WAVE1_EVENT = pygame.USEREVENT
+TARGET_WAVE1_EVENT = pygame.USEREVENT + 1
 
-TARGET_WAVE2_EVENT = pygame.USEREVENT
+TARGET_WAVE2_EVENT = pygame.USEREVENT + 2
+
+TARGET_WAVE3_EVENT = pygame.USEREVENT + 3
 
 TARGET_PADDING = 30
 
@@ -39,16 +25,19 @@ LIVES = 3
 TOP_BAR_HEIGTH = 50
 
 LABEL_FONT = pygame.font.SysFont("comicsans", 24)
+GAMEOVER_FONT = pygame.font.SysFont("consolas", 70, bold=True)
 LABEL_COLOR = "white"
 
 class Target:
     ISBONUS = False
     MAX_SIZE = 30
     BONUS_MAX_SIZE = 50
+    GOLD_MAX_SIZE = 20
     GROWTH_RATE = 0.2
     
     COLOR = "red"
     BONUS_COLOR = "green"
+    GOLD_COLOR = (255, 215, 0)
     SECOND_COLOR = "white"
 
     def __init__(self, x, y):
@@ -56,13 +45,21 @@ class Target:
         self.y = y
         self.size = 0
         self.grow = True
-        BONUS = random.randint(0,10)
-        if BONUS == 10:
-            self.ISBONUS = True
+        self.ISBONUS = random.randint(0, 10) == 10
+        if not self.ISBONUS:
+            self.ISGOLD = random.randint(0, 20) == 20
 
     def update(self):
-        if not self.ISBONUS:
-            if self.size + self.GROWTH_RATE >= self.MAX_SIZE:
+        if self.ISBONUS:
+            if self.size + self.GROWTH_RATE >= self.BONUS_MAX_SIZE:
+                self.grow = False
+
+            if self.grow:
+                self.size += self.GROWTH_RATE
+            else:
+                self.size -= self.GROWTH_RATE
+        elif self.ISGOLD:
+            if self.size + self.GROWTH_RATE >= self.GOLD_MAX_SIZE:
                 self.grow = False
 
             if self.grow:
@@ -70,7 +67,7 @@ class Target:
             else:
                 self.size -= self.GROWTH_RATE
         else:
-            if self.size + self.GROWTH_RATE >= self.BONUS_MAX_SIZE:
+            if self.size + self.GROWTH_RATE >= self.MAX_SIZE:
                 self.grow = False
 
             if self.grow:
@@ -79,26 +76,32 @@ class Target:
                 self.size -= self.GROWTH_RATE
     
     def draw(self, FEN):
-        if not self.ISBONUS:
-            pygame.draw.circle(FEN, self.COLOR, (self.x, self.y), self.size)
-            pygame.draw.circle(FEN, self.SECOND_COLOR, (self.x, self.y), self.size * 0.8)
-            pygame.draw.circle(FEN, self.COLOR, (self.x, self.y), self.size * 0.6)
-            pygame.draw.circle(FEN, self.SECOND_COLOR, (self.x, self.y), self.size * 0.4)
-        else:
+        if self.ISBONUS:
             pygame.draw.circle(FEN, self.BONUS_COLOR, (self.x, self.y), self.size)
             pygame.draw.circle(FEN, self.SECOND_COLOR, (self.x, self.y), self.size * 0.8)
             pygame.draw.circle(FEN, self.BONUS_COLOR, (self.x, self.y), self.size * 0.6)
             pygame.draw.circle(FEN, self.SECOND_COLOR, (self.x, self.y), self.size * 0.4)
+        elif self.ISGOLD:
+            pygame.draw.circle(FEN, self.GOLD_COLOR, (self.x, self.y), self.size)
+            pygame.draw.circle(FEN, self.SECOND_COLOR, (self.x, self.y), self.size * 0.8)
+            pygame.draw.circle(FEN, self.GOLD_COLOR, (self.x, self.y), self.size * 0.6)
+            pygame.draw.circle(FEN, self.SECOND_COLOR, (self.x, self.y), self.size * 0.4)
+        else:
+            pygame.draw.circle(FEN, self.COLOR, (self.x, self.y), self.size)
+            pygame.draw.circle(FEN, self.SECOND_COLOR, (self.x, self.y), self.size * 0.8)
+            pygame.draw.circle(FEN, self.COLOR, (self.x, self.y), self.size * 0.6)
+            pygame.draw.circle(FEN, self.SECOND_COLOR, (self.x, self.y), self.size * 0.4)
+        
 
     def collide(self, x, y):
         dis = math.sqrt((self.x - x)**2 + (self.y - y)**2)
         return dis <= self.size
     
     def isbonus(self):
-        if self.ISBONUS:
-            return True
-        else:
-            return False
+        return self.ISBONUS
+    
+    def isgold(self):    
+        return self.ISGOLD
 
 def draw(FEN, targets):
     FEN.fill(BG_COLOR)
@@ -133,40 +136,72 @@ def draw_top_bar(FEN, elapsed_time, targets_pressed, misses, wave, score):
     FEN.blit(lives_label, (575, 5))
     FEN.blit(wave_label, (700, 5))
 
+def save_game_data(file_name, data):
+    df = pd.DataFrame([data])
+    if os.path.exists(file_name):
+        df.to_csv(file_name, mode='a', header=False, index=False)
+        
+        df_existing = pd.read_csv(file_name)
+        best_score = df_existing['Score'].max() if 'Score' in df_existing.columns else 0
+        
+        # Création d'un DataFrame pour le meilleur score
+        best = pd.DataFrame({'Best Score': [f'Best Score: {best_score}']})
+        best.to_csv(file_name, mode='a', header=False, index=False)
+
+        bestacc = df_existing['Accuracy'].max() if 'Accuracy' in df_existing.columns else 0
+
+        best_accuracy = pd.DataFrame({'Best Accuracy': [f'Best Accuracy: {bestacc}']})
+        best_accuracy.to_csv(file_name, mode='a', header=False, index=False)
+
+
+    else:
+        df.to_csv(file_name, index=False)
+
+
 def end_screen(FEN, elapsed_time, targets_pressed, clicks, score):
+    print("Entering end_screen function")
     FEN.fill(BG_COLOR)
     time_label = LABEL_FONT.render(f"Time: {format_time(elapsed_time)}", 1, LABEL_COLOR)
 
-    speed = round(targets_pressed / elapsed_time, 1)
+    speed = round(targets_pressed / elapsed_time, 1) if elapsed_time > 0 else 0
     speed_label = LABEL_FONT.render(f"Speed: {speed} t/s", 1, LABEL_COLOR)
 
     score_label = LABEL_FONT.render(f"Score: {score}", 1, LABEL_COLOR)
 
-    accuracy = round(targets_pressed / clicks * 100, 1)
+    accuracy = round(targets_pressed / clicks * 100, 1) if clicks >= 15 else 0
     accuracy_label = LABEL_FONT.render(f"Accuracy: {accuracy}%", 1, LABEL_COLOR)
 
-    FEN.blit(time_label, (get_middle(time_label), 100))
-    FEN.blit(speed_label, (get_middle(speed_label), 200))
-    FEN.blit(score_label, (get_middle(score_label), 300))
-    FEN.blit(accuracy_label, (get_middle(accuracy_label), 400))
+    gameover_label = GAMEOVER_FONT.render(f"GAMEOVER !", 1, LABEL_COLOR)
+
+    FEN.blit(gameover_label, (get_middle(gameover_label), 75))
+    FEN.blit(time_label, (get_middle(time_label), 200))
+    FEN.blit(speed_label, (get_middle(speed_label), 300))
+    FEN.blit(score_label, (get_middle(score_label), 400))
+    FEN.blit(accuracy_label, (get_middle(accuracy_label), 500))
 
     pygame.display.update()
     
-    game_data = pd.DataFrame({
-        'Time': [format_time(elapsed_time)],
-        'Targets Pressed': [targets_pressed],
-        'Clicks': [clicks],
-        'Score': [score],
-        'Accuracy': [accuracy]
-    })
-    
+    game_data = {
+        'Time': format_time(elapsed_time),
+        'Targets Pressed': targets_pressed,
+        'Clicks': clicks,
+        'Score': score,
+        'Accuracy': accuracy
+    }
     save_game_data('game_data.csv', game_data)
 
+    print("End screen displayed, entering loop to wait for user action")
     run = True
     while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT or event.type == pygame.KEYDOWN:
-                quit()
+                run = False
+                break
+        pygame.time.delay(100)  # Small delay to reduce CPU usage
+
+    pygame.quit()
+    print("Exiting end_screen function")
+    quit()
         
 def get_middle(surface):
     return WIDTH / 2 - surface.get_width()/2
@@ -176,6 +211,7 @@ def main():
     targets = []
     clock = pygame.time.Clock()
 
+    actual_lives = LIVES
     score = 0
     targets_pressed = 0
     clicks = 0
@@ -185,12 +221,13 @@ def main():
     wave = 0
     wave1 = False
     wave2 = False
+    wave3 = False
 
     pygame.time.set_timer(TARGET_EVENT, TARGET_INCREMENT)
     
     while run:
         run = True
-  
+
         clock.tick(60)
         click = False
         mouse_pos = pygame.mouse.get_pos()
@@ -201,7 +238,7 @@ def main():
                 run = False
                 break
 
-            if event.type == TARGET_EVENT or event.type == TARGET_WAVE1_EVENT or event.type == TARGET_WAVE2_EVENT:
+            if event.type == TARGET_EVENT or event.type == TARGET_WAVE1_EVENT or event.type == TARGET_WAVE2_EVENT or event.type == TARGET_WAVE3_EVENT:
                 x = random.randint(TARGET_PADDING, WIDTH - TARGET_PADDING)
                 y = random.randint(TARGET_PADDING + TOP_BAR_HEIGTH, HEIGHT - TARGET_PADDING)
                 target = Target(x, y)
@@ -216,18 +253,26 @@ def main():
 
             if target.size <= 0:
                 targets.remove(target)
-                misses += 1
+                if target.isgold():
+                    misses += 3
+                else:
+                    misses += 1
             
             if click and target.collide(*mouse_pos):
                 targets.remove(target)
                 if target.isbonus():
                     score += 5
+                    misses -= 1
+                elif target.isgold():
+                    score += 10
+                    misses -= 3
                 else:
                     score += 1
                 targets_pressed += 1
         
-        if misses >= LIVES:
+        if misses >= actual_lives:
             end_screen(FEN, elapsed_time, targets_pressed, clicks, score)
+            break
             
         if elapsed_time >= 15 and not wave1:
             pygame.time.set_timer(TARGET_EVENT, 0)
@@ -237,8 +282,15 @@ def main():
         
         if elapsed_time >= 30 and not wave2:
             pygame.time.set_timer(TARGET_WAVE1_EVENT, 0)
-            pygame.time.set_timer(TARGET_WAVE2_EVENT, TARGET_INCREMENT-400)
+            pygame.time.set_timer(TARGET_WAVE2_EVENT, TARGET_INCREMENT-300)
             wave2 = True
+            wave += 1
+        
+        if elapsed_time >= 45 and not wave3:
+            pygame.time.set_timer(TARGET_WAVE1_EVENT, 0)
+            pygame.time.set_timer(TARGET_WAVE2_EVENT, 0)
+            pygame.time.set_timer(TARGET_WAVE3_EVENT, TARGET_INCREMENT-400)
+            wave3 = True
             wave += 1
         
         draw(FEN, targets)
